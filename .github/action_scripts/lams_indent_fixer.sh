510e8331fc049268911389309bc9ecd258687f60
Hafiza Neha Shahid <49759692+hnehashahid@users.noreply.github.com>
2025-07-24 14:04:55 +0200

    Update lams_indent_fixer.sh

--- .github/action_scripts/lams_indent_fixer.sh
+++ .github/action_scripts/lams_indent_fixer.sh
@@ -3,5 +3,3 @@ git config user.name "github-actions"
 git config user.email "github-actions@github.com"
 git commit --allow-empty -m "Trigger CI"
 git push https://x-access-token:${TOKEN}@github.com/${GH_REPO} HEAD:DHUB-4098-adding-auto-fix-indentation-workflow
-
-
